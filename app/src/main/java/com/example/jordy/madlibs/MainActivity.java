fa7777ff40db95ca0dc8464c9fe7b1ee520c701f
11433124 <jordy_hierck@live.nl>
2016-11-08 16:57:53 +0100

    Added extra xml files. Added initial code for inital button.

--- app/src/main/java/com/example/jordy/madlibs/MainActivity.java
+++ app/src/main/java/com/example/jordy/madlibs/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.jordy.madlibs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void GetStarted(View view) {
+        setContentView(R.layout.fillable);
+
+    }
+
 }
